9ce48aa04b4dc2b8213f38a5e21caafca8143fed
Cristian Hurtado <chiplinelgnomo@gmail.com>
2024-11-22 18:49:19 +0100

    Ventana de login añadida junto a su estilo

--- funciones.py
+++ funciones.py
@@ -11,13 +11,6 @@ def login():
 
 
 
-
-
-
-        
-
-
-
 def aplicar_stylesheet(widget, filename):      
     """Aplica los estilos CSS desde un archivo."""
     file = QFile(filename)
@@ -36,23 +29,22 @@ def aplicar_stylesheet(widget, filename):
 
 
 
-def crear_login(self):
+def crear_login(self):          #* Funcion que crea la vista del login, con sus layouts, botones y demás
     # Crear el widget de login
-    self.login = QWidget(self)  # Asegúrate de crear el widget
-
-    layout_login = QVBoxLayout()  # Renombrado a layout_login
+    self.login = QWidget(self)  
+    layout_login = QVBoxLayout()  
 
     # Fondo y cartel en login
-    self.carteltienda_login = QLabel(self)  # Renombrado a carteltienda_login
+    self.carteltienda_login = QLabel(self)  
     pixmap2 = QPixmap('grand_blue.png')
     self.carteltienda_login.setPixmap(pixmap2)
     self.carteltienda_login.setAlignment(Qt.AlignTop | Qt.AlignHCenter)
     self.carteltienda_login.setFixedSize(400, 200)
     self.carteltienda_login.setScaledContents(True)
-
     layout_cartel_login = QVBoxLayout()
     layout_cartel_login.addWidget(self.carteltienda_login)
     layout_cartel_login.setAlignment(Qt.AlignTop | Qt.AlignHCenter)
+    layout_cartel_login.setContentsMargins(29, 0, 0, 0)
 
     # Campos de usuario y contraseña
     self.usuario_input = QLineEdit(self)
@@ -60,21 +52,35 @@ def crear_login(self):
     self.contraseña_input = QLineEdit(self)
     self.contraseña_input.setPlaceholderText("Contraseña")
     self.contraseña_input.setEchoMode(QLineEdit.Password)
-
-    # Botón de login
+    self.usuario_input.setFixedSize(160, 50)
+    self.contraseña_input.setFixedSize(160, 50)
     self.boton_login = QPushButton("Iniciar sesión", self)
-
-    # Añadir widgets al layout de login
+    self.boton_login.clicked.connect(login)
+    self.boton_login.setFixedSize(160, 50)
+    self.usuario_input.setObjectName("cuadrousuario")
+    self.contraseña_input.setObjectName("cuadrocontra")
+    self.boton_login.setObjectName("botonlogin")
+    layout_botones_login = QVBoxLayout()
+    layout_botones_login.addWidget(self.usuario_input)
+    layout_botones_login.addWidget(self.contraseña_input)
+    layout_botones_login.addWidget(self.boton_login)
+    layout_botones_login.setSpacing(20)
+    layout_botones_login.setContentsMargins(150, 0, 0, 100)
     layout_login.addLayout(layout_cartel_login)
-    layout_login.addWidget(self.usuario_input)
-    layout_login.addWidget(self.contraseña_input)
+    layout_login.addLayout(layout_botones_login)
     layout_login.addWidget(self.boton_login)
-    layout_login.setAlignment(Qt.AlignCenter)
+    layout_login.setAlignment(Qt.AlignTop | Qt.AlignHCenter)
+    layout_login.setSpacing(50)
+    layout_login.setContentsMargins(70, 200, 70, 0)
+    
 
     self.login.setLayout(layout_login)  # Aquí se configura el layout para 'login'
 
 
 
 
-def mostrar_login(self):
-        self.stacked_widget.setCurrentIndex(1)
+def mostrar_login(self): #* Esta funcion solamente cambia la pagina de la vista actual a la de login
+        # Cambiar al login
+        self.stacked_widget.setCurrentWidget(self.login)
+
+
--- main.py
+++ main.py
@@ -2,89 +2,87 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QStackedWidget
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
-from funciones import *  # Solo importamos la función de login
+from funciones import *  
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
 
-        # Configuración de la ventana
+        #! ===========================>Ventana principal<=========================== !# 
         self.setWindowTitle("Grand Blue")
         self.setFixedSize(600, 700)
         self.setObjectName("ventana_principal")
         
-        # Fondo de la ventana
+        
+        #* Aqui se crea un label para la imagen de fondo usando pixmap y se configura para que se adapte a la ventana principal
         background_label = QLabel(self)
         pixmap = QPixmap('fondoapp2.png')
         background_label.setPixmap(pixmap)
         background_label.setScaledContents(True)
         background_label.setGeometry(0, 0, self.width(), self.height())
 
-        # Crear el stacked_widget
-        self.stacked_widget = QStackedWidget(self)
-
+        
+        #! ===========================>Vista inicial<=========================== !#
         # Crear la vista principal (ya está creada en el constructor)
         self.vista_principal = QWidget(self)
+        layout_principal = QVBoxLayout(self.vista_principal) #* Layout que contiene a su vez los layouts de los botones y el cartel
+        self.login = QWidget(self)
+        crear_login(self.login) 
+        
 
-        # Layout de la vista principal
-        layout_principal = QVBoxLayout(self.vista_principal)
-
-        # Código para el cartel
+        #* Codigo para el cartel de grand blue
         self.carteltienda = QLabel(self)
         pixmap2 = QPixmap('grand_blue.png')
         self.carteltienda.setPixmap(pixmap2)
         self.carteltienda.setAlignment(Qt.AlignTop | Qt.AlignHCenter)
         self.carteltienda.setFixedSize(400, 200)
         self.carteltienda.setScaledContents(True)
-
-        # Layout para el cartel
         layout_cartel = QVBoxLayout()
         layout_cartel.addWidget(self.carteltienda)
         layout_cartel.setAlignment(Qt.AlignTop | Qt.AlignHCenter)  # Alineación del cartel en la parte superior central
+        layout_principal.addLayout(layout_cartel)
+        layout_principal.addSpacing(20)
 
-        # Código para los botones
+        #* Código para los botones
         self.botonempleados = QPushButton("Zona Empleados", self)
         self.botonclientes = QPushButton("Zona Clientes", self)
         self.botonclientes.setObjectName("botonclientes")
         self.botonempleados.setObjectName("botonempleados")
         self.botonclientes.setFixedSize(160, 50)
         self.botonempleados.setFixedSize(160, 50)
-        self.botonempleados.clicked.connect(self.mostrar_login)
-
-        # Layout para los botones
+        self.botonempleados.clicked.connect(lambda: mostrar_login(self))  #* Funcion que se llama al clicar para que vista cambie a otra (login)
         layout_botones = QVBoxLayout()
         layout_botones.addWidget(self.botonempleados)
         layout_botones.addWidget(self.botonclientes)
         layout_botones.setAlignment(Qt.AlignCenter)  # Centrar los botones
-        layout_botones.setContentsMargins(50, 50, 50, 130)
-        layout_botones.setSpacing(50)
-
-        # Añadir el layout del cartel y espaciado entre el cartel y los botones
-        layout_principal.addLayout(layout_cartel)
-        layout_principal.addSpacing(20)  # Controla la distancia entre el cartel y los botones
-
-        # Añadir el layout de los botones al layout principal
-        layout_principal.addLayout(layout_botones)
+        layout_botones.setSpacing(20)
+        layout_principal.addLayout(layout_botones) #* Aqui se añade el layout de los botones al layout principal para unirlo con el del cartel
         layout_principal.setContentsMargins(70, 200, 50, 0)  # Márgenes de la ventana
 
-        # Añadir la vista principal al stacked_widget
-        self.stacked_widget.addWidget(self.vista_principal)
 
-        # Crear la vista de login
-        self.login = QWidget(self)
-        crear_login(self.login)  # Llamamos a la función que crea la vista de login
-        self.stacked_widget.addWidget(self.login)  # Agregamos la vista de login al stacked_widget
+         
+        
+        
+
+        #! ===========================>StackedWidget<=========================== !#
+        self.stacked_widget = QStackedWidget(self)    #* Aqui se crea el StackedWidget
+        self.stacked_widget.addWidget(self.vista_principal) #* Aqui se añade la vista_principal creada al inicio como widget, que es igual a toda la ventana inicial, el cartel y los botones.
+        self.stacked_widget.addWidget(self.login) #* Se añade la vista de login al stackedwidget despues de crearse.
+        
 
         # Añadir el stacked_widget al layout principal
         layout_principal_general = QVBoxLayout(self)
         layout_principal_general.addWidget(self.stacked_widget)
-        self.setLayout(layout_principal_general)  # Asignamos el layout principal
+        self.setLayout(layout_principal_general)  
 
+        #* Aqui se crea la vista de login como widget, se llama a la funcion que la crea con sus layouts y widgets y se añade al stacked
+        
+
+        #* Aqui se llama a la funcion que aplica los estilos de css a los respectivos widgets
         aplicar_stylesheet(self, 'estilos.css')
 
-    def mostrar_login(self):
-        # Cambiar al login
-        self.stacked_widget.setCurrentWidget(self.login)
+
+    
 
 # Ejecutar la aplicación
 if __name__ == "__main__":
